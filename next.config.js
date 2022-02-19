aeb63c40075437af87462a4e46bbfcf4a059b64e
Akhmad Lylana <lanaemel19@gmail.com>
2022-02-19 10:41:45 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_SERVER_URL: "https://5w1wgzbemm9f.bigmoralis.com:2053/server",
+    NEXT_PUBLIC_APP_ID: "7uQ481cNxNDgFLv3YqzzM76X7avmta4kivEpAi5t",
+    TOKEN_V1: "0xc98a8ec7a07f1b743e86896a52434c4c6a0dbc42",
+    TOKEN_V2: "0xe51cfcff691acb4d7bfcee3a87ff215132c66ef7"
+  },
+};
